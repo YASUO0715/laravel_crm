2cea531d97147ae6492f042b4feae928ae883563
YASUO0715 <yasuo0715niihori@gmail.com>
2021-09-27 12:30:17 +0900

    index(view)を作成しました。

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,36 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 
 class CustomerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // /**
+    //  * Display a listing of the resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
     public function index()
     {
-        $method = 'GET';
-        $zip_code = '7830060';
-        // QIITA_URLの値を取得してURLを定義
-        $url = config('zipcode.url'). '/api/search?zipcode=/'.$zip_code;
-
-        // Client(接続する為のクラス)を生成
-        $client = new Client();
-        // try catchでエラー時の処理を書く
-        try {
-            // データを取得し、JSON形式からPHPの変数に変換
-            $response = $client->request($method, $url);
-            $body = $response->getBody();
-            $customers = json_decode($body, false);
-        } catch (\Throwable $th) {
-            $customers = null;
-        }
-        return view('customers.index')->with(compact('customers'));
-
+        $customers = Customer::all();
+        // Itemsティレクトリーの中のindexページを指定し、itemsの連想配列を代入
+        return view('customers.index', ['customers' => $customers]);
     }
 
     /**
--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,22 +1,48 @@
-
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>auction index</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
+
 <body>
     <h1>顧客一覧</h1>
-    @if (!empty($customers))
-        <ul>
-            @foreach ($customers as $customer)
-                <li>
-                    {{ $customer->zipcode }}
-                </li>
-            @endforeach
-        </ul>
-    @endif
+    <ul>
+        <div class="big_table">
+            <table border="1">
+                <tr>
+                    <th>顧客ID</th>
+                    <th>名前</th>
+                    <th>メールアドレス</th>
+                    <th>郵便番号</th>
+                    <th>住所</th>
+                    <th>電話番号</th>
+                </tr>
+                @foreach ($customers as $customer)
+
+                    <table border='1'>
+                        <tr>
+                            
+                            <td><a href="/customers/{{ $customer->id }}">{{ $customer->id }}</a></td>
+                            <td class="cell2">{{ $customer->name }}</td>
+                            <td class="cell3">{{ $customer->email }}</td>
+                            <td class="cell4">{{ $customer->zipcode }}</td>
+                            <td class="cell5">{{ $customer->address }}</td>
+                            <td class="cell6">{{ $customer->phoneNumber }}</td>
+                        </tr>
+                    </table>
+        </div>
+
+        @endforeach
+    </ul>
+
+    <button><a href="/planets/create">新規登録</a></button>
+
+
 </body>
+
 </html>
